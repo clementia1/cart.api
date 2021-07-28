f53c0926d670ffafaca92893c98c5145842537c4
clementia1 <grininma0@gmail.com>
2021-07-28 13:28:37 +0300

    Update Startup.cs

--- src/CartApi/Startup.cs
+++ src/CartApi/Startup.cs
@@ -69,7 +69,7 @@ namespace CartApi
             });
             app.UseRouting();
 
-            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod());
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseEndpoints(builder => builder.MapDefaultControllerRoute());
         }
